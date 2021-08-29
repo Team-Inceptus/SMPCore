fffe895e354097529f59ed5c2d62d5247b2f57c9
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2021-08-28 21:29:08 -0500

    Update DeltaCave.java
    Local Updates

--- src/gamercoder215/smpcore/listeners/caves/DeltaCave.java
+++ src/gamercoder215/smpcore/listeners/caves/DeltaCave.java
@@ -1,11 +1,34 @@
 package gamercoder215.smpcore.listeners.caves;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 import gamercoder215.smpcore.Main;
 
-public class DeltaCave extends Cave {
-
+public class DeltaCave extends Cave implements Listener {
+	
 	public DeltaCave(Main plugin) {
 		super(plugin, null, 0);
+		Bukkit.getPluginManager().registerEvents(this, plugin);
+	}
+	
+	public static ItemStack getDeltaStone() {
+		ItemStack dStone = new ItemStack(Material.STONE, 1);
+		ItemMeta dMeta = dStone.getItemMeta();
+		dMeta.setDisplayName(ChatColor.DARK_GRAY + "Delta Stone");
+		dStone.setItemMeta(dMeta);
+		
+		return dStone;
+	}
+	
+	public static ItemStack getDeltarack() {
+		ItemStack drack = new ItemStack(Material.NETHERRACK, 1);
+		
+		return drack;
 	}
 
 }
