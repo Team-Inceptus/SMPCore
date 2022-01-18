d8e05687447d6e8bd31ab12a11128f1af0d62de8
GamerCoder215 <gamercoder215@gmail.com>
2022-01-17 20:12:05 -0600

    Update SMPCore.java
    Fixed Worlds not Loading Bug & Added Mute Command

--- src/us/teaminceptus/smpcore/SMPCore.java
+++ src/us/teaminceptus/smpcore/SMPCore.java
@@ -12,7 +12,10 @@ import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -84,6 +87,7 @@ import us.teaminceptus.smpcore.commands.Hat;
 import us.teaminceptus.smpcore.commands.Help;
 import us.teaminceptus.smpcore.commands.InvSee;
 import us.teaminceptus.smpcore.commands.Menu;
+import us.teaminceptus.smpcore.commands.Mute;
 import us.teaminceptus.smpcore.commands.RankUp;
 import us.teaminceptus.smpcore.commands.Rejoin;
 import us.teaminceptus.smpcore.commands.SetRank;
@@ -91,6 +95,7 @@ import us.teaminceptus.smpcore.commands.SpawnCustomEntity;
 import us.teaminceptus.smpcore.commands.Suspend;
 import us.teaminceptus.smpcore.commands.Suspendlist;
 import us.teaminceptus.smpcore.commands.TitanWarps;
+import us.teaminceptus.smpcore.commands.UnMute;
 import us.teaminceptus.smpcore.commands.Value;
 import us.teaminceptus.smpcore.commands.WandInfo;
 import us.teaminceptus.smpcore.commands.WorldChat;
@@ -241,6 +246,10 @@ public class SMPCore extends JavaPlugin {
 	    	  playerConfig.getConfigurationSection("npc_talks").set("bellator", false);;
 	      }
 	      
+	      if (!(playerConfig.isBoolean("muted"))) {
+	    	  playerConfig.set("muted", false);
+	      }
+	      
 	      try {
 	    	 playerConfig.save(playerFile);
 	      } catch (IOException e) {
@@ -275,192 +284,192 @@ public class SMPCore extends JavaPlugin {
 	  }.runTaskTimer(this, 20 * (r.nextInt(60 - 30) + 30), 20 * (r.nextInt(60 - 30) + 30));
 	  
 	  // Load External Worlds
-//	  // Titans
-//	  WorldCreator titanWorld = new WorldCreator("world_titan");
-//	  Bukkit.createWorld(titanWorld);
-//	  
-//	  WorldCreator titanWorldNether = new WorldCreator("world_titan_nether");
-//	  Bukkit.createWorld(titanWorldNether);
-//	  
-//	  WorldCreator titanWorldEnd = new WorldCreator("world_titan_end");
-//	  Bukkit.createWorld(titanWorldEnd);
-//	  
-//	  // Caves
-//	  WorldCreator deltaCave = new WorldCreator("world_caves_delta");
-//	  Bukkit.createWorld(deltaCave);
-//	  
-//	  WorldCreator alphaCave = new WorldCreator("world_caves_alpha");
-//	  Bukkit.createWorld(alphaCave);
-//	  
-//	  WorldCreator titanCave = new WorldCreator("world_caves_titan");
-//	  Bukkit.createWorld(titanCave);
-//	  
-//	  // Planatae
-//	  WorldCreator hubP = new WorldCreator("world_planatae_hub");
-//	  Bukkit.createWorld(hubP);
-//	  
-//	  WorldCreator gammaP = new WorldCreator("world_planatae_gamma");
-//	  Bukkit.createWorld(gammaP);
-//	  
-//	  WorldCreator omegaP = new WorldCreator("world_planatae_omega");
-//	  Bukkit.createWorld(omegaP);
-//	  
-//	  WorldCreator titanP = new WorldCreator("world_planatae_titan");
-//	  Bukkit.createWorld(titanP);
-//	  
-//	  // World-Specific Modifications
-//	  new BukkitRunnable() {
-//		  public void run() {
-//			  Bukkit.getOnlinePlayers().forEach(p -> {
-//				  if (p.getWorld().getName().equalsIgnoreCase("world_titan_end")) {
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 4, 6, true, false, false));
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 4, 1, true, false, false));
-//				  }
-//				  
-//				  if (p.getWorld().getName().equalsIgnoreCase("world_titan_nether") || p.getWorld().getName().equalsIgnoreCase("world_titan_nether")) {
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 3, true, false, false));
-//				  }
-//				  
-//				  if (p.getWorld().getName().contains("caves")) {
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 4, 1, true, false, false));
-//				  }
-//				  
-//				  if (p.getWorld().getName().equalsIgnoreCase("world_caves_delta")) {
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 0, true, false, false));
-//				  }
-//				  
-//				  if (p.getWorld().getName().equalsIgnoreCase("world_caves_alpha")) {
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 3, true, false, false));
-//				  }
-//				  
-//				  if (p.getWorld().getName().equalsIgnoreCase("world_caves_titan")) {
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 6, true, false, false));
-//				  }
-//				  
-//				  if (p.getWorld().getName().equalsIgnoreCase("world_planatae_gamma")) {
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 1, true, false, false));
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 4, 2, true, false, false));
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 4, 2, true, false, false));
-//				  }
-//				  
-//				  if (p.getWorld().getName().equalsIgnoreCase("world_planatae_omega")) {
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 4, true, false, false));
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 4, 5, true, false, false));
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 4, 5, true, false, false));
-//				  }
-//				  
-//				  if (p.getWorld().getName().equalsIgnoreCase("world_planatae_titan")) {
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 7, true, false, false));
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 4, 9, true, false, false));
-//					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 4, 9, true, false, false));
-//				  }
-//			  });
-//			 
-//		  }
-//	  }.runTaskTimer(this, 0, 2);
-//	  
-//	  // Chalc Ability - Omega Planatae
-//	  World cW = Bukkit.getWorld("world_planatae_omega");
-//	  Location[] rodLocations = {
-//			  new Location(cW, -432, 199, 252),
-//			  new Location(cW, -426, 211, 237),
-//			  new Location(cW, -415, 216, 211),
-//			  new Location(cW, -390, 206, 184),
-//			  new Location(cW, -359, 218, 193),
-//			  new Location(cW, -356, 222, 211),
-//			  new Location(cW, -338, 222, 226),
-//			  new Location(cW, -323, 220, 243),
-//			  new Location(cW, -322, 222, 266),
-//			  new Location(cW, -342, 226, 263),
-//			  new Location(cW, -351, 221, 278),
-//			  new Location(cW, -371, 231, 290),
-//			  new Location(cW, -384, 222, 277),
-//			  new Location(cW, -387, 222, 293),
-//			  new Location(cW, -397, 224, 281),
-//			  new Location(cW, -396, 222, 260),
-//			  new Location(cW, -386, 224, 241),
-//			  new Location(cW, -376, 224, 229),
-//			  new Location(cW, -387, 222, 213),
-//			  new Location(cW, -396, 222, 213),
-//			  new Location(cW, -314, 207, 198),
-//			  new Location(cW, -314, 215, 207),
-//			  new Location(cW, -293, 200, 204),
-//			  new Location(cW, -285, 198, 223),
-//			  new Location(cW, -290, 214, 245),
-//			  new Location(cW, -291, 220, 250),
-//			  new Location(cW, -295, 205, 282),
-//			  new Location(cW, -305, 197, 304),
-//			  new Location(cW, -333, 203, 320),
-//			  new Location(cW, -356, 207, 323),
-//			  new Location(cW, -375, 211, 317),
-//			  new Location(cW, -394, 212, 306),
-//			  new Location(cW, -392, 218, 300),
-//			  new Location(cW, -370, 220, 305),
-//			  new Location(cW, -351, 222, 301),
-//			  new Location(cW, -325, 221, 293),
-//			  new Location(cW, -329, 222, 282),
-//			  new Location(cW, -328, 224, 272),
-//			  new Location(cW, -335, 220, 254),
-//			  new Location(cW, -356, 222, 240),
-//			  new Location(cW, -362, 222, 246),
-//			  new Location(cW, -371, 233, 269),
-//			  new Location(cW, -374, 245, 282),
-//			  new Location(cW, -365, 222, 257),
-//			  new Location(cW, -370, 222, 237),
-//			  new Location(cW, -334, 223, 210),
-//			  new Location(cW, -304, 219, 242),
-//			  new Location(cW, -307, 215, 283),
-//			  new Location(cW, -317, 217, 293),
-//			  new Location(cW, -336, 223, 286),
-//			  new Location(cW, -345, 230, 262),
-//			  new Location(cW, -381, 223, 264),
-//			  new Location(cW, -392, 222, 272),
-//			  new Location(cW, -395, 224, 275),
-//			  new Location(cW, -409, 220, 258),
-//			  new Location(cW, -421, 215, 253),
-//			  new Location(cW, -425, 208, 275),
-//			  new Location(cW, -408, 195, 312),
-//			  new Location(cW, -377, 172, 342),
-//			  new Location(cW, -361, 179, 337),
-//			  new Location(cW, -344, 190, 330),
-//			  new Location(cW, -312, 187, 319),
-//			  new Location(cW, -298, 191, 307),
-//			  new Location(cW, -280, 182, 285),
-//			  new Location(cW, -283, 196, 264),
-//			  new Location(cW, -282, 200, 226),
-//			  new Location(cW, -303, 218, 256),
-//			  new Location(cW, -325, 222, 254),
-//			  new Location(cW, -349, 221, 236),
-//			  new Location(cW, -348, 222, 225),
-//			  new Location(cW, -365, 220, 213),
-//			  new Location(cW, -372, 220, 197),
-//			  new Location(cW, -386, 215, 195),
-//			  new Location(cW, -418, 210, 212),
-//			  new Location(cW, -403, 220, 234),
-//			  new Location(cW, -382, 222, 253),
-//			  new Location(cW, -373, 228, 266),
-//			  new Location(cW, -370, 238, 280),
-//			  new Location(cW, -361, 222, 288),
-//			  new Location(cW, -337, 222, 280),
-//			  new Location(cW, -334, 221, 265),
-//			  new Location(cW, -337, 222, 239),
-//			  new Location(cW, -322, 222, 227),
-//			  new Location(cW, -326, 222, 217),
-//			  new Location(cW, -354, 220, 224),
-//			  new Location(cW, -349, 223, 212),
-//			  new Location(cW, -395, 222, 237),
-//			  new Location(cW, -404, 222, 241),
-//			  new Location(cW, -413, 221, 261),
-//			  new Location(cW, -410, 220, 249),
-//			  new Location(cW, -411, 221, 231),
-//	  };
-//	  
-//	  new BukkitRunnable() {
-//		  public void run() {
-//			  cW.strikeLightning(rodLocations[r.nextInt(rodLocations.length)]);
-//			  cW.strikeLightning(rodLocations[r.nextInt(rodLocations.length)]);
-//		  }
-//	  }.runTaskTimer(this, 0, 20 * (r.nextInt(30) + 7));
+	  // Titans
+	  WorldCreator titanWorld = new WorldCreator("world_titan");
+	  Bukkit.createWorld(titanWorld);
+	  
+	  WorldCreator titanWorldNether = new WorldCreator("world_titan_nether");
+	  Bukkit.createWorld(titanWorldNether);
+	  
+	  WorldCreator titanWorldEnd = new WorldCreator("world_titan_end");
+	  Bukkit.createWorld(titanWorldEnd);
+	  
+	  // Caves
+	  WorldCreator deltaCave = new WorldCreator("world_caves_delta");
+	  Bukkit.createWorld(deltaCave);
+	  
+	  WorldCreator alphaCave = new WorldCreator("world_caves_alpha");
+	  Bukkit.createWorld(alphaCave);
+	  
+	  WorldCreator titanCave = new WorldCreator("world_caves_titan");
+	  Bukkit.createWorld(titanCave);
+	  
+	  // Planatae
+	  WorldCreator hubP = new WorldCreator("world_planatae_hub");
+	  Bukkit.createWorld(hubP);
+	  
+	  WorldCreator gammaP = new WorldCreator("world_planatae_gamma");
+	  Bukkit.createWorld(gammaP);
+	  
+	  WorldCreator omegaP = new WorldCreator("world_planatae_omega");
+	  Bukkit.createWorld(omegaP);
+	  
+	  WorldCreator titanP = new WorldCreator("world_planatae_titan");
+	  Bukkit.createWorld(titanP);
+	  
+	  // World-Specific Modifications
+	  new BukkitRunnable() {
+		  public void run() {
+			  Bukkit.getOnlinePlayers().forEach(p -> {
+				  if (p.getWorld().getName().equalsIgnoreCase("world_titan_end")) {
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 4, 6, true, false, false));
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 4, 1, true, false, false));
+				  }
+				  
+				  if (p.getWorld().getName().equalsIgnoreCase("world_titan_nether") || p.getWorld().getName().equalsIgnoreCase("world_titan_nether")) {
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 3, true, false, false));
+				  }
+				  
+				  if (p.getWorld().getName().contains("caves")) {
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 4, 1, true, false, false));
+				  }
+				  
+				  if (p.getWorld().getName().equalsIgnoreCase("world_caves_delta")) {
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 0, true, false, false));
+				  }
+				  
+				  if (p.getWorld().getName().equalsIgnoreCase("world_caves_alpha")) {
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 3, true, false, false));
+				  }
+				  
+				  if (p.getWorld().getName().equalsIgnoreCase("world_caves_titan")) {
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 6, true, false, false));
+				  }
+				  
+				  if (p.getWorld().getName().equalsIgnoreCase("world_planatae_gamma")) {
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 1, true, false, false));
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 4, 2, true, false, false));
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 4, 2, true, false, false));
+				  }
+				  
+				  if (p.getWorld().getName().equalsIgnoreCase("world_planatae_omega")) {
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 4, true, false, false));
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 4, 5, true, false, false));
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 4, 5, true, false, false));
+				  }
+				  
+				  if (p.getWorld().getName().equalsIgnoreCase("world_planatae_titan")) {
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 4, 7, true, false, false));
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 4, 9, true, false, false));
+					  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 4, 9, true, false, false));
+				  }
+			  });
+			 
+		  }
+	  }.runTaskTimer(this, 0, 2);
+	  
+	  // Chalc Ability - Omega Planatae
+	  World cW = Bukkit.getWorld("world_planatae_omega");
+	  Location[] rodLocations = {
+			  new Location(cW, -432, 199, 252),
+			  new Location(cW, -426, 211, 237),
+			  new Location(cW, -415, 216, 211),
+			  new Location(cW, -390, 206, 184),
+			  new Location(cW, -359, 218, 193),
+			  new Location(cW, -356, 222, 211),
+			  new Location(cW, -338, 222, 226),
+			  new Location(cW, -323, 220, 243),
+			  new Location(cW, -322, 222, 266),
+			  new Location(cW, -342, 226, 263),
+			  new Location(cW, -351, 221, 278),
+			  new Location(cW, -371, 231, 290),
+			  new Location(cW, -384, 222, 277),
+			  new Location(cW, -387, 222, 293),
+			  new Location(cW, -397, 224, 281),
+			  new Location(cW, -396, 222, 260),
+			  new Location(cW, -386, 224, 241),
+			  new Location(cW, -376, 224, 229),
+			  new Location(cW, -387, 222, 213),
+			  new Location(cW, -396, 222, 213),
+			  new Location(cW, -314, 207, 198),
+			  new Location(cW, -314, 215, 207),
+			  new Location(cW, -293, 200, 204),
+			  new Location(cW, -285, 198, 223),
+			  new Location(cW, -290, 214, 245),
+			  new Location(cW, -291, 220, 250),
+			  new Location(cW, -295, 205, 282),
+			  new Location(cW, -305, 197, 304),
+			  new Location(cW, -333, 203, 320),
+			  new Location(cW, -356, 207, 323),
+			  new Location(cW, -375, 211, 317),
+			  new Location(cW, -394, 212, 306),
+			  new Location(cW, -392, 218, 300),
+			  new Location(cW, -370, 220, 305),
+			  new Location(cW, -351, 222, 301),
+			  new Location(cW, -325, 221, 293),
+			  new Location(cW, -329, 222, 282),
+			  new Location(cW, -328, 224, 272),
+			  new Location(cW, -335, 220, 254),
+			  new Location(cW, -356, 222, 240),
+			  new Location(cW, -362, 222, 246),
+			  new Location(cW, -371, 233, 269),
+			  new Location(cW, -374, 245, 282),
+			  new Location(cW, -365, 222, 257),
+			  new Location(cW, -370, 222, 237),
+			  new Location(cW, -334, 223, 210),
+			  new Location(cW, -304, 219, 242),
+			  new Location(cW, -307, 215, 283),
+			  new Location(cW, -317, 217, 293),
+			  new Location(cW, -336, 223, 286),
+			  new Location(cW, -345, 230, 262),
+			  new Location(cW, -381, 223, 264),
+			  new Location(cW, -392, 222, 272),
+			  new Location(cW, -395, 224, 275),
+			  new Location(cW, -409, 220, 258),
+			  new Location(cW, -421, 215, 253),
+			  new Location(cW, -425, 208, 275),
+			  new Location(cW, -408, 195, 312),
+			  new Location(cW, -377, 172, 342),
+			  new Location(cW, -361, 179, 337),
+			  new Location(cW, -344, 190, 330),
+			  new Location(cW, -312, 187, 319),
+			  new Location(cW, -298, 191, 307),
+			  new Location(cW, -280, 182, 285),
+			  new Location(cW, -283, 196, 264),
+			  new Location(cW, -282, 200, 226),
+			  new Location(cW, -303, 218, 256),
+			  new Location(cW, -325, 222, 254),
+			  new Location(cW, -349, 221, 236),
+			  new Location(cW, -348, 222, 225),
+			  new Location(cW, -365, 220, 213),
+			  new Location(cW, -372, 220, 197),
+			  new Location(cW, -386, 215, 195),
+			  new Location(cW, -418, 210, 212),
+			  new Location(cW, -403, 220, 234),
+			  new Location(cW, -382, 222, 253),
+			  new Location(cW, -373, 228, 266),
+			  new Location(cW, -370, 238, 280),
+			  new Location(cW, -361, 222, 288),
+			  new Location(cW, -337, 222, 280),
+			  new Location(cW, -334, 221, 265),
+			  new Location(cW, -337, 222, 239),
+			  new Location(cW, -322, 222, 227),
+			  new Location(cW, -326, 222, 217),
+			  new Location(cW, -354, 220, 224),
+			  new Location(cW, -349, 223, 212),
+			  new Location(cW, -395, 222, 237),
+			  new Location(cW, -404, 222, 241),
+			  new Location(cW, -413, 221, 261),
+			  new Location(cW, -410, 220, 249),
+			  new Location(cW, -411, 221, 231),
+	  };
+	  
+	  new BukkitRunnable() {
+		  public void run() {
+			  cW.strikeLightning(rodLocations[r.nextInt(rodLocations.length)]);
+			  cW.strikeLightning(rodLocations[r.nextInt(rodLocations.length)]);
+		  }
+	  }.runTaskTimer(this, 0, 20 * (r.nextInt(30) + 7));
 	  
 	  // Update Player Inventory (Rarity)
 	  new BukkitRunnable() {
@@ -630,6 +639,8 @@ public class SMPCore extends JavaPlugin {
       new Suspendlist(this);
       new GetStatistic(this);
       new SetRank(this);
+      new Mute(this);
+      new UnMute(this);
       // Listeners & Utils
       new PlayerStatusUpdate(this);
       new Cosmetics(this);
